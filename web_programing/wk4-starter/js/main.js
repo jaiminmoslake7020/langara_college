5d3a9d4817e7fa3e1a88755d007b9f914de91475
jaimin pandya <jaiminmoslake7020@gmail.com>
2019-10-06 18:46:46 -0700

    NEW MATERIALS

--- web_programing/wk4-starter/js/main.js
+++ web_programing/wk4-starter/js/main.js
@@ -1,122 +1,91 @@
-// alert("It works Jaimin");
+let myArray = [] ;
 
-//*************************************************************************** 
+console.log( "Line 3 is working" );
+//alert( "Line 3 is working" );
+
+//***************************************************************************
 //***************************************************************************
 //
 // Code for Implementing Color Picker
 //
-//*************************************************************************** 
-//*************************************************************************** 
+//***************************************************************************
+//***************************************************************************
+
+box1bgColor.addEventListener('change', (event) => {
+    console.log(event);
+    sec2.style.backgroundColor = box1bgColor.value;
 
 
-box1bgColor.addEventListener('change', ( event ) => {
-    console.log( event );
-    sec2.style.backgroundColor = box1bgColor.value ;
 });
 
-//*************************************************************************** 
+console.log( "Line 18 is working" );
+//alert( "Line 18 is working" );
+
+
+//***************************************************************************
 //***************************************************************************
 //
 // Code for Implementing Input Box
 //
-//*************************************************************************** 
-//*************************************************************************** 
+//***************************************************************************
+//***************************************************************************
 
-
-box2.addEventListener('click', ( event ) => { 
-
-    console.log( event );
-    console.log( event['target'] );
-
-    console.log( box2txtField.value );
-
+box2.addEventListener('click', (event) => {
+    console.log(event);
+    console.log(event['target']);
+    console.log(box2txtField.value);
     output2.innerHTML = `The email address you provided was: ${box2txtField.value}`;
+
 
 });
 
+console.log( "Line 36 is working" );
+//alert( "Line 36 is working" );
 
-//*************************************************************************** 
+
+//***************************************************************************
 //***************************************************************************
 //
 // Code for Implementing Check Box
 //
-//*************************************************************************** 
-//*************************************************************************** 
+//***************************************************************************
+//***************************************************************************
 
-
-box3chkBox.addEventListener('change', ( event ) => {
-    console.log( box3chkBox.value );
-
-    
-    if( box3chkBox.checked ){
-        output3.innerHTML += "<br/>Great! You've been add to ouyr list - Thank You!" ;
-    }else{
-        let joiningMeeting = box3chkBox.checked ? "" : "not" ;
+box3chkBox.addEventListener('change', (event) => {
+    console.log(box3chkBox.value);
+    if (box3chkBox.checked) {
+        output3.innerHTML += "<br/>Great! You've been add to ouyr list - Thank You!";
+    } else {
+        let joiningMeeting = box3chkBox.checked ? "" : "not";
         output3.innerHTML += `<br/>Please remember to click the checkbox to join our mailing list.`;
     }
 
 });
 
+x=y;
 
-const judy = "Judy" ;
-const mon = "Mon" ;
+console.log( "Line 57 is working" );
+//alert( "Line 57 is working" );
 
 
-let sponsorsArraybyPerson = [] ;
 
-sponsorsArraybyPerson[judy] = [] ;
-sponsorsArraybyPerson[judy] = [15,3,11,7,17] ;
 
-sponsorsArraybyPerson[mon] = [];
-sponsorsArraybyPerson[mon] = [8,8,13,10,11] ;
 
-let getTotalSponsorsByArray = ( person ) => {
-    if( typeof sponsorsArraybyPerson === "object" && sponsorsArraybyPerson.hasOwnProperty( person ) ){
-        let sponsorsArrayOfPerson = sponsorsArraybyPerson[person];
-        let totalNumberOfSponsersForThisPerson = 0 ;
-        for (let index = 0; index < sponsorsArrayOfPerson.length; index++) {
-             totalNumberOfSponsersForThisPerson += sponsorsArrayOfPerson[index];
-        }   
-        return totalNumberOfSponsersForThisPerson;
-    }
-    return 0 ;
-};
-
-let alertWhoHasWon = () => {
-
-    let totalNumberOfSponsersForJudy = getTotalSponsorsByArray( judy );
-    let totalNumberOfSponsersForMon = getTotalSponsorsByArray( mon );
-    let winner  ;
-    let looser  ;
-    let winnerNoOfSponsors ;
-    let looserNoOfSponsors ;
-    let noOfSponsorsInCaseOfTie  ;
-    let alertString ;
-
-    if( totalNumberOfSponsersForJudy > totalNumberOfSponsersForMon  ){
-        winner = judy ;
-        looser = mon ;
-        winnerNoOfSponsors = totalNumberOfSponsersForJudy;
-        looserNoOfSponsors = totalNumberOfSponsersForMon;
-    }else if( totalNumberOfSponsersForJudy < totalNumberOfSponsersForMon  ){
-        winner = mon ;
-        looser = judy ;
-        winnerNoOfSponsors = totalNumberOfSponsersForMon;
-        looserNoOfSponsors = totalNumberOfSponsersForJudy;
-    }else {
-        winner = "none" ;
-        noOfSponsorsInCaseOfTie = totalNumberOfSponsersForMon;
-    }
-
-    if( winner != "none" ){
-        alertString = `${winner} has won the bet. As ${winner} was able to attract ${winnerNoOfSponsors} sponsors and ${looser} was able to attract ${looserNoOfSponsors} sponsors. ${winner} has ${(winnerNoOfSponsors-looserNoOfSponsors)} more sponsores compared to ${looser}.` ;
+box4.addEventListener('click',() => {
+    myArray.push( parseFloat(box4txtField.value) );
+    if( myArray.length > 1 ){
+        let gradeAvg = 0 ;
+        for ( let i = 0 ; i < myArray.length ; i++ ){
+            gradeAvg += myArray[i];
+        }
+        gradeAvg /= myArray.length ;
+        output4.innerHTML = `The average of grades entered is ${gradeAvg}` ;
     }else{
-        alertString = `It is a tie between ${judy} and ${mon}. Both of them got ${noOfSponsorsInCaseOfTie} sponsors.` ;
+        output4.innerHTML = `You only entered one grade and that grade is  ${myArray[0]}` ;
     }
 
-    alert(alertString);
-};
+    output4.innerHTML += '<br><br>'+myArray;
+});
 
-alertWhoHasWon();
 
 
